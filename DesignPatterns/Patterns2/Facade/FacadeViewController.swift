bb87fbcdbd421751814346f5bef6ebe132b44953
ta9yamakawa <myttari1206@gmail.com>
2021-08-15 18:43:10 +0900

    facadeのVC作成

---
+++ DesignPatterns/Patterns2/Facade/FacadeViewController.swift
@@ -0,0 +1,16 @@
+//
+//  FacadeViewController.swift
+//  DesignPatterns
+//
+//  Created by ta9yamakawa on 2021/08/15.
+//
+
+import UIKit
+
+/// FacadeViewController
+final class FacadeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
